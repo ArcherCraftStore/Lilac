2f6212522a1b5188265125b27500d36accb87682
WH <weldon23.henson23@gmail.com>
2014-05-17 19:30:32 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import cmd,sys,os
+from datetime import *
+
+
+class Lilac(cmd.Cmd):
+ def __init__(self):
+  cmd.Cmd.__init__(self)
+  self.prompt="ArcherCraft VM"
+ def do_tipcalc(self,line):
+  meal = raw_input("Enter the meal cost")
+  tax = raw_input("Enter the tax")
+  tip = raw_input("enter the Tip")
+
+  meal = meal + meal * tax
+
+  total = meal + meal * tip
+  print("%.2f" % total)
+
+
+if __name__  = "__main__":
+  archercraftvmlilac = Lilac()
+  archercraftvmlilac.cmdloop()
